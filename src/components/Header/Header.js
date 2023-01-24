d10c21f05b0f94d75bc1329a7dcf022e9772fd8d
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-24 12:12:11 -0800

    useParams grabs rgb values from URL and renders Main

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,18 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <ul className="color-bar">
-      <NavLink to="/rgb/213/155/69">
-        <li className="mustard">mustard</li>
-      </NavLink>
-      <NavLink to="/rgb/104/158/216">
-        <li className="periwinkle">periwinkle</li>
-      </NavLink>
-      <NavLink to="/rgb/129/134/74">
-        <li className="olive">olive</li>
-      </NavLink>
-    </ul>
+    <nav>
+      <ul className="color-bar">
+        <NavLink to="/rgb/213/155/69">
+          <li className="mustard">mustard</li>
+        </NavLink>
+        <NavLink to="/rgb/104/158/216">
+          <li className="periwinkle">periwinkle</li>
+        </NavLink>
+        <NavLink to="/rgb/129/134/74">
+          <li className="olive">olive</li>
+        </NavLink>
+      </ul>
+    </nav>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Main() {
+  const { red, green, blue } = useParams();
   return (
-    <div>
-      <h1>rgb()</h1>
-    </div>
+    <main>
+      <h1 style={{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }}>
+        rgb({red}, {green}, {blue})
+      </h1>
+    </main>
   );
 }
